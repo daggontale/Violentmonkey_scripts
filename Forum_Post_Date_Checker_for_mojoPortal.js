f6ab013978387a8cca3dbd2c2e6195cc85949cc4
daggontale <60053466+daggontale@users.noreply.github.com>
2022-10-27 08:29:44 -0500

    Add files via upload

---
+++ Forum_Post_Date_Checker_for_mojoPortal.js
@@ -0,0 +1,73 @@
+// ==UserScript==
+// @name        Post Date Checker for mojoPortal.com
+// @namespace   https://github.com/daggontale/ViolentMonkey-Scripts/blob/main/Post_Date_Checker_for_mojoPortal.com.js
+// @match       https://www.mojoportal.com/forums*
+// @grant       none
+// @license     MIT
+// @version     1.0
+// @author      Joey Stombaugh
+// @description Checks mojoportal forums for a new post and notifies if so
+// ==/UserScript==
+
+const style = document.createElement('style');
+style.innerHTML = '.card{position:fixed;top:50%;transform:translate(0,-50%);right:10px;border:1px solid gray;border-radius:5px;max-width:200px;background-color: aliceblue}.card__time{font-size:larger;text-align:center;border-bottom:1px solid gray;padding:5px 5px 3px}.card__body{padding:5px}';
+document.head.appendChild(style);
+
+const dateElementNodeList = document.querySelectorAll('.fpostdate');
+
+const dateTitleArray = Array.from(dateElementNodeList).map(element => {
+
+  const title = element.parentNode.querySelector('.ftitle > h3 > a:nth-child(3)')?.textContent;
+
+  return {
+      title,
+      date: new Date(element.textContent)
+  }
+});
+
+function removeDay(daysToRemove) {
+  return new Date(new Date().getTime() - (8.64e+7 * daysToRemove));
+}
+
+const datesFromYesterdayArray = dateTitleArray.filter(x => {
+  if (x.date > removeDay(1)) {
+    return true;
+  }
+  return false;
+});
+
+const datesFromThisWeekArray = dateTitleArray.filter(x => {
+  if (x.date > removeDay(5) && !datesFromYesterdayArray.includes(x)) {
+      return true;
+  }
+  return false;
+});
+
+const currentPosts = []
+const refreshTime = new Intl.DateTimeFormat('en-US', {timeStyle: 'short', hourCycle: 'h12' }).format(Date.now());
+datesFromYesterdayArray.forEach(x => currentPosts.push(`New Post to ${x.title} in the last 24 hours. (${x.date.getMonth() + 1}/${x.date.getDate()}/${x.date.getFullYear()})`));
+datesFromThisWeekArray.forEach(x => currentPosts.push(`New Post to ${x.title} in the past week. (${x.date.getMonth() + 1}/${x.date.getDate()}/${x.date.getFullYear()})`));
+
+const notif = document.createElement('div');
+notif.classList.add('card');
+
+const cardTime = document.createElement('div');
+cardTime.classList.add('card__time');
+cardTime.innerText = refreshTime;
+
+const posts = document.createElement('div');
+posts.classList.add('card__body');
+
+if (currentPosts.length == 0) {
+	posts.innerText = 'No new posts.';
+} else {
+	posts.innerText = currentPosts;
+}
+
+document.body.appendChild(notif);
+notif.appendChild(cardTime);
+notif.appendChild(posts);
+
+setTimeout(() => {
+  location.reload();
+}, 1800000);
---
+++ WHMCS_Ticket_Watcher.js
@@ -0,0 +1,74 @@
+// ==UserScript==
+// @name        WHMCS Ticket Watcher
+// @namespace   https://github.com/daggontale/WHMCS-Ticket-Watcher/blob/main/WHMCS_Ticket_Watcher.js
+// @match       https://support.i7media.com/nv79/supporttickets.php
+// @grant       none
+// @version     2.1
+// @author      Joey Stombaugh
+// @description Creates a node on the side of the screen displaying all forums with new threads
+// ==/UserScript==
+
+// The constant definitions are in all caps just to help with case sensitivity.
+
+// Watches for these ticket statuses.
+const STATUS_LIST = ["CUSTOMER-REPLY", "OPEN"];
+
+// Only looks for tickets with these assigned departments. Will look through all if empty.
+const DEPARTMENT_LIST = ["TECHNICAL SUPPORT"];
+
+// Must be a string. Ticket ids that will never be listed. Usually for tickets that joe and phillip are taking care of, but are not assigned directly to them.
+const BLACKLIST = [];
+
+
+function parseLastReply(string) {
+	const days = /\d+(?=d)/.exec(string) * 8.64e+7;
+	const hours = /\d+(?=h)/.exec(string) * 3.6e+6;
+	const minutes = /\d+(?=m)/.exec(string) * 60000;
+
+	return days + hours + minutes;
+}
+
+function createTicketObj(ticketNode) {
+	return {
+		dept: ticketNode.childNodes[2].textContent.toUpperCase().trim(),
+		element: ticketNode,
+		id: ticketNode.childNodes[3].innerText.slice(5, 10).trim(),
+		lastReply: parseLastReply(ticketNode.childNodes[6].innerText),
+		status: ticketNode.childNodes[5].textContent.toUpperCase().trim()
+	};
+};
+
+function ticketFilter(ticketObject) {
+	return (!BLACKLIST?.includes(ticketObject.id) && STATUS_LIST.includes(ticketObject.status) && DEPARTMENT_LIST.includes(ticketObject.dept));
+};
+
+const MATCHING_TICKETS = Array.from(document.querySelectorAll('[id*="sortabletbl"] > tbody > tr:not(:first-child)'))
+	.map(x => createTicketObj(x))
+	.filter(ticketFilter);
+
+for (const ticket of MATCHING_TICKETS) {
+	ticket.element.children[3].style.backgroundColor = 'yellow';
+
+	if (ticket.lastReply < 4.32e+7) {
+		ticket.element.children[6].style.backgroundColor = 'lime';
+	} else if (ticket.lastReply >= 4.32e+7 && ticket.lastReply < 8.64e+7) {
+		ticket.element.children[6].style.backgroundColor = 'yellow';
+	} else if (ticket.lastReply >= 8.64e+7 && ticket.lastReply < 1.728e+8) {
+		ticket.element.children[6].style.backgroundColor = 'orange';
+	} else if (ticket.lastReply >= 1.728e+8) {
+		ticket.element.children[6].style.backgroundColor = 'red';
+		setInterval(() => {
+			ticket.element.children[6].style.backgroundColor = 'yellow';
+			setTimeout(() => { ticket.element.children[6].style.backgroundColor = 'red'; }, 500);
+		}, 1000)
+	}
+}
+
+console.log(MATCHING_TICKETS)
+
+if (MATCHING_TICKETS.length > 0) {
+	setTimeout(() => { alert('New Tickets') }, 60000);
+}
+
+// just a different test
+console.log("Hello, world! AAAAAAAAAAAHHHHHHHHHHHHHHHHHHHH");
