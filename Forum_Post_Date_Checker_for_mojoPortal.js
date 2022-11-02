3d686d8367a6db7646bc49bbac879ceebde4f4c1
daggontale <60053466+daggontale@users.noreply.github.com>
2022-11-01 19:07:11 -0500

    Added feature: date blacklist
    The forums on mojoportal have a bug where the forum doesnt update its latest time when you delete a thread, so i added a way to manually do it.

--- Forum_Post_Date_Checker_for_mojoPortal.js
+++ Forum_Post_Date_Checker_for_mojoPortal.js
@@ -6,8 +6,13 @@
 // @license     MIT
 // @version     1.0
 // @author      Joey Stombaugh
-// @description Creates a node on the side of the screen displaying all forums with new threads
+// @description Checks mojoportal forums for a new post and notifies if so
 // ==/UserScript==
+
+// Variables
+var DATE_BLACKLIST = ["10/30/2022", "11/1/2022"];
+
+// Functions
 
 function removeDay(daysToRemove) {
 	return new Date(new Date().getTime() - (8.64e+7 * daysToRemove));
@@ -16,24 +21,26 @@ function removeDay(daysToRemove) {
 function createNotificationNode(timeRefreshed, displayArr) {
 	const notif = document.createElement('div');
 	notif.classList.add('card');
-	
+
 	const cardTime = document.createElement('div');
 	cardTime.classList.add('card__time');
 	cardTime.innerText = timeRefreshed;
-	
+
 	const posts = document.createElement('div');
 	posts.classList.add('card__body');
-	
+
 	if (displayArr.length == 0) {
-		posts.innerText = 'No new posts.';
+		posts.innerHTML = 'No new posts.';
 	} else {
-		posts.innerText = displayArr;
+		posts.innerHTML = displayArr;
 	}
-	
+
 	document.body.appendChild(notif);
 	notif.appendChild(cardTime);
 	notif.appendChild(posts);
 }
+
+// Code
 
 const style = document.createElement('style');
 style.innerHTML = '.card{position:fixed;top:50%;transform:translate(0,-50%);right:10px;border:1px solid gray;border-radius:5px;max-width:200px;background-color: aliceblue}.card__time{font-size:larger;text-align:center;border-bottom:1px solid gray;padding:5px 5px 3px}.card__body{padding:5px}';
@@ -48,27 +55,24 @@ const dateTitleArray = Array.from(dateElementNodeList).map(element => {
   return {
       title,
       date: new Date(element.textContent)
-  }
+  };
 });
 
+console.log(dateTitleArray);
+
 const datesFromYesterdayArray = dateTitleArray.filter(x => {
-  if (x.date > removeDay(1)) {
-    return true;
-  }
-  return false;
+  return new Date(x.date) > removeDay(1) && !DATE_BLACKLIST.includes(x.date.toLocaleDateString());
 });
 
 const datesFromThisWeekArray = dateTitleArray.filter(x => {
-  if (x.date > removeDay(5) && !datesFromYesterdayArray.includes(x)) {
-      return true;
-  }
-  return false;
+  return new Date(x.date) > removeDay(5) && !datesFromYesterdayArray.includes(x) && !DATE_BLACKLIST.includes(x.date.toLocaleDateString());
 });
 
 const currentPosts = []
 const refreshTime = new Intl.DateTimeFormat('en-US', {timeStyle: 'short', hourCycle: 'h12' }).format(Date.now());
-datesFromYesterdayArray.forEach(x => currentPosts.push(`New Post to ${x.title} in the last 24 hours. (${x.date.getMonth() + 1}/${x.date.getDate()}/${x.date.getFullYear()})`));
-datesFromThisWeekArray.forEach(x => currentPosts.push(`New Post to ${x.title} in the past week. (${x.date.getMonth() + 1}/${x.date.getDate()}/${x.date.getFullYear()})`));
+
+datesFromYesterdayArray.forEach(x => currentPosts.push(`New Post to <strong>${x.title}</strong> in the last 24 hours. (${x.date.toLocaleDateString()})`));
+datesFromThisWeekArray.forEach(x => currentPosts.push(`New Post to <strong>${x.title}</strong> in the past week. (${x.date.toLocaleDateString()})`));
 
 createNotificationNode(refreshTime, currentPosts);
 
