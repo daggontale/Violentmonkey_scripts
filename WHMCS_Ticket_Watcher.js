4aaf54fdc5b7410f86c439831a8545a51573f752
daggontale <60053466+daggontale@users.noreply.github.com>
2022-11-02 14:40:27 -0500

    Added blacklist tab to webpage
    Instead of adding the blacklisted ticket ids on the violentmonkey edit page, you can add them to the localstorage via the new ticket blacklist tab at the top of the page.

--- WHMCS_Ticket_Watcher.js
+++ WHMCS_Ticket_Watcher.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name        WHMCS Ticket Watcher
-// @namespace   https://github.com/daggontale/Violentmonkey_scripts
+// @namespace   https://github.com/daggontale/WHMCS-Ticket-Watcher/blob/main/WHMCS_Ticket_Watcher.js
 // @match       https://support.i7media.com/nv79/supporttickets.php
 // @grant       none
-// @version     2.2
+// @version     2.3
 // @author      Joey Stombaugh
 // @description Highlights new tickets and notifies window after 1 minute.
 // ==/UserScript==
@@ -16,9 +16,139 @@ const STATUS_LIST = ["CUSTOMER-REPLY", "OPEN"];
 // Only looks for tickets with these assigned departments. Will look through all if empty.
 const DEPARTMENT_LIST = ["TECHNICAL SUPPORT"];
 
-// Must be a string. Ticket ids that will never be listed. Usually for tickets that joe and phillip are taking care of, but are not assigned directly to them.
-const BLACKLIST = [];
+// Add tickets to the blacklist via the custom ticket blacklist tab on the webpage.
+const BLACKLIST = JSON.parse(localStorage.getItem('ticketBlacklist', '[]'))?.map(x => x.ticketId);
 
+// Functions --------------------------------------------------------------------------------------------------------------------------------------------
+
+function ticketBlacklistTab(adminTabNodeList) {
+  const blacklistCache = JSON.parse(localStorage.getItem('ticketBlacklist', '[]'));
+  const newTab = document.createElement('li');
+
+  newTab.innerHTML = `<a class="tab-top" href="#tab${adminTabNodeList[0].childElementCount + 1}" role="tab" data-toggle="tab" id="tabLink${adminTabNodeList[0].childElementCount + 1}" data-tab-id="${adminTabNodeList[0].childElementCount + 1}">Ticket Blacklist</a>`;
+  adminTabNodeList[0].appendChild(newTab);
+
+  const tabPage = document.createElement('div');
+
+  tabPage.classList += "tab-pane";
+  tabPage.id = `tab${adminTabNodeList[1].childElementCount + 1}`;
+
+  const wrapper = document.createElement('div');
+  wrapper.align = 'center';
+  const input = document.createElement('input');
+  input.classList.add('form-control', 'select-inline');
+  input.type = 'text';
+  input.placeholder = 'Ticket Id';
+  input.id = 'input-blacklist';
+  const addToList = document.createElement('button');
+  addToList.textContent = 'Add';
+  addToList.classList = 'btn btn-primary btn-sm';
+  addToList.id = 'btn-blacklist';
+  addToList.addEventListener('click', x => {
+    if (!blacklistInput.value == '' && !blacklistCache.includes(input.value)) {
+      blacklistCache.push({ timeAdded: new Date().toLocaleDateString(), ticketId: input.value });
+      localStorage.ticketBlacklist = JSON.stringify(blacklistCache);
+    }
+  })
+  const showList = document.createElement('button');
+  showList.textContent = 'Show Blacklist';
+  showList.classList = 'btn btn-primary btn-sm';
+  showList.id = 'btn-show-blacklist';
+  showList.addEventListener('click', () => showBlacklistedTickets(blacklistTabTable) )
+
+  const tableWrapper = document.createElement('div');
+  tableWrapper.align = 'center';
+  tableWrapper.id = 'blacklistTabTable'
+
+  wrapper.appendChild(input);
+  wrapper.appendChild(addToList);
+  wrapper.appendChild(showList);
+
+  tabPage.appendChild(wrapper);
+  tabPage.appendChild(tableWrapper);
+
+  adminTabNodeList[1].appendChild(tabPage);
+}
+
+function showBlacklistedTickets(node) {
+  const tableElement = document.querySelector('#blacklistTable');
+
+  if (tableElement) {
+    tableElement.remove()
+  }
+
+  const blacklistTable = document.createElement('table');
+  blacklistTable.id = 'blacklistTable'
+  blacklistTable.classList += 'datatable'
+
+  const tableHead = document.createElement('thead');
+  const headRow = document.createElement('tr');
+  const header1 = document.createElement('th');
+  header1.textContent = 'Date Added';
+  const header2 = document.createElement('th');
+  header2.textContent = 'Ticket Id';
+  const header3 = document.createElement('th');
+  header3.textContent = 'Controls';
+
+  headRow.append(header1, header2, header3);
+  tableHead.appendChild(headRow);
+
+  const tableBody = document.createElement('tbody');
+  const blacklistedTickets = JSON.parse(localStorage.ticketBlacklist);
+
+  if (blacklistedTickets == []) {
+    const bodyRow = document.createElement('tr');
+    const noTickets = document.createElement('td');
+    noTickets.textContent = 'No Blacklisted Tickets';
+
+    bodyRow.appendChild(noTickets);
+    tableBody.appendChild(bodyRow);
+  } else {
+    for (let ticket of blacklistedTickets) {
+      const bodyRow = document.createElement('tr');
+      const col1 = document.createElement('td');
+      col1.textContent = ticket.timeAdded;
+      const col2 = document.createElement('td');
+      col2.textContent = ticket.ticketId;
+      const col3 = document.createElement('td');
+      const removeBtn = document.createElement('button');
+      removeBtn.addEventListener('click', () => {
+        removeTicketFromBlacklist(ticket.ticketId);
+        showBlacklistedTickets(blacklistTabTable);
+      });
+      removeBtn.textContent = 'remove from list';
+
+      col3.appendChild(removeBtn);
+      bodyRow.append(col1, col2, col3);
+      tableBody.appendChild(bodyRow);
+    }
+  }
+
+  blacklistTable.append(tableHead, tableBody);
+
+  node.appendChild(blacklistTable);
+}
+
+function getDayDifference(date, days) {
+  dateToCheck = new Date(date);
+  xDaysAgo = new Date(new Date().getTime() + (86400000 * days));
+  if (xDaysAgo.getTime() < dateToCheck) {
+    return true;
+  }
+  return false;
+}
+
+function removeOldTickets() {
+  const filteredTickets = JSON.parse(localStorage.getItem('ticketBlacklist', '[]'))
+    .filter(x => getDayDifference(x.timeAdded, -5));
+  localStorage.setItem('ticketBlacklist', JSON.stringify(filteredTickets));
+}
+
+function removeTicketFromBlacklist(id) {
+  const filteredTickets = JSON.parse(localStorage.getItem('ticketBlacklist', '[]'))
+    .filter(x => !(x.ticketId === id));
+  localStorage.setItem('ticketBlacklist', JSON.stringify(filteredTickets));
+}
 
 function parseLastReply(string) {
 	const days = /\d+(?=d)/.exec(string) * 8.64e+7;
@@ -36,11 +166,24 @@ function createTicketObj(ticketNode) {
 		lastReply: parseLastReply(ticketNode.childNodes[6].innerText),
 		status: ticketNode.childNodes[5].textContent.toUpperCase().trim()
 	};
-}
+};
 
 function ticketFilter(ticketObject) {
 	return (!BLACKLIST?.includes(ticketObject.id) && STATUS_LIST.includes(ticketObject.status) && DEPARTMENT_LIST.includes(ticketObject.dept));
-}
+};
+
+// Code --------------------------------------------------------------------------------------------------------------------------------------------
+
+const adminTabs = document.querySelectorAll('.admin-tabs');
+
+ticketBlacklistTab(adminTabs);
+
+const blacklistInput = document.querySelector('#input-blacklist');
+const blacklistButton = document.querySelector('#btn-blacklist');
+const showBlacklistButton = document.querySelector('#btn-show-blacklist');
+const blacklistTabTable = document.querySelector('#blacklistTabTable');
+
+removeOldTickets();
 
 const MATCHING_TICKETS = Array.from(document.querySelectorAll('[id*="sortabletbl"] > tbody > tr:not(:first-child)'))
 	.map(x => createTicketObj(x))
